46c67bb4096fa1a3158f2a82f1d744aa367216ff
Voltron7 <aquastar.minsk@gmail.com>
2022-11-11 15:15:18 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,36 +5,60 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
-    public static void main(String[] args) throws IOException {
-        for (int page = 1; page <= 5; page++) {
-            String currentPage = String.format("%s%d", PAGE_LINK, page);
-            Connection connection = Jsoup.connect(currentPage);
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                Element dateElement = row.select(".vacancy-card__date").first();
-                Element dateLink = dateElement.child(0);
-                String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                String dateTime = String.format("%s", dateLink.attr("datetime"));
-                String description = retrieveDescription(link);
-                HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
-                LocalDateTime localDateTime = parser.parse(dateTime);
-                System.out.printf("%s %s %s %s%n", vacancyName, localDateTime, link, description);
-            });
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    public static void main(String[] args) {
+        HabrCareerParse parser = new HabrCareerParse(new HabrCareerDateTimeParser());
+        List<Post> list = parser.list(PAGE_LINK);
+        for (Post vacancy : list) {
+            System.out.println(vacancy);
         }
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> list = new ArrayList<>();
+        try {
+            for (int page = 1; page <= 5; page++) {
+                String currentPage = String.format("%s%d", link, page);
+                Connection connection = Jsoup.connect(currentPage);
+                Document document = connection.get();
+                Elements rows = document.select(".vacancy-card__inner");
+                rows.forEach(row -> {
+                    Element titleElement = row.select(".vacancy-card__title").first();
+                    Element linkElement = titleElement.child(0);
+                    Element dateElement = row.select(".vacancy-card__date").first();
+                    Element dateLink = dateElement.child(0);
+                    String vacancyName = titleElement.text();
+                    String postLink = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                    String dateTime = String.format("%s", dateLink.attr("datetime"));
+                    String description = retrieveDescription(postLink);
+                    HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+                    LocalDateTime localDateTime = parser.parse(dateTime);
+                    list.add(new Post(vacancyName, postLink, description, localDateTime));
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     private static String retrieveDescription(String link) {
